daffd19082ad240bfbb67cda655ace57fdb58634
kaczka <manfil28@gmail.com>
2024-03-14 17:51:58 +0100

    parkuje na recznym pod bankiem audi

---
+++ backend/warships/src/test/java/org/example/warships/WarshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.warships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarshipsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
