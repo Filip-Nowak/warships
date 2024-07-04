8e5f8c85b83ddff819d1f46eed4728af390579bb
kaczka <manfil28@gmail.com>
2024-07-04 19:29:56 +0200

    na ulicach ciagle cicho

--- front/warships/src/components/utils/Menu/Menu.js
+++
@@ -1,8 +0,0 @@
-import styles from "./menu.module.css"
-function Menu({containerStyle,children,className}){
-    return <div className={styles.container+" "+className} style={containerStyle}>
-        {children}
-    </div>
-
-}
-export default Menu;
--- front/warships/src/components/utils/SubmitMessageInput/SubmitMessageInput.js
+++
@@ -1,9 +0,0 @@
-import SubmitInput from "../submitInput/SubmitInput";
-import styles from "./submit.module.css"
-function SubmitMessageInput({message,buttonMessage,handleButtonClick,containerStyle,messageStyle,inputContainerStyle,buttonMessageStyle,inputStyle}){
-    return <div style={containerStyle}>
-        <div className={styles.message} style={messageStyle}>{message}</div>
-        <SubmitInput inputStyle={inputStyle} buttonMessageStyle={buttonMessageStyle} msg={buttonMessage} handleButtonClick={handleButtonClick} containerStyle={inputContainerStyle}></SubmitInput>
-    </div>
-}
-export default SubmitMessageInput
